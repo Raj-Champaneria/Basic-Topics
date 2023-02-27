910a38dd1a3439b22f8543eb57fe5c52afc38d7a
Raj Champaneria <raj.champaneria@1Rintern.local>
2023-02-27 18:10:00 +0530

    register form aded

---
+++ JavaScript/Practise_folder/Cricket crud/Assests/script/script.js
@@ -0,0 +1,42 @@
+let btnregister = document.getElementById("save");
+
+btnregister.addEventListener("click", (e) => {
+  e.preventDefault();
+  addPlayer();
+  window.location.href = "table.html";
+});
+
+function addPlayer() {
+  let fname = document.getElementById("firstname").value;
+  let lname = document.getElementById("lastname").value;
+  let age = document.getElementById("age").value;
+  let email = document.getElementById("email").value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let gender = document.querySelector('input[name="season"]:checked').value;
+  let ch = document.querySelector('input[name="color"]:checked').value;
+  // let values = [];
+
+  // ch.forEach((ch) => {
+  //   values.push(ch.value);
+  // });
+
+  fetch("http://localhost:3000/cricket", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      fname,
+      lname,
+      age,
+      email,
+      gender,
+      ch,
+      username,
+      password,
+    }),
+  })
+    .then((Response) => Response.json())
+    .then((data) => console.log(data));
+}
---
+++ JavaScript/Practise_folder/Cricket crud/Assests/script/table.js
@@ -0,0 +1,83 @@
+let table = document.createElement("table");
+table.classList.add("table-style");
+
+let headers = [
+  { header: "ID", key: "id" },
+  { header: "First Name", key: "fname" },
+  { header: "Last Name", key: "lname" },
+  { header: "Age", key: "age" },
+  { header: "gender", key: "gender" },
+  { header: "checked Item", key: "ch" },
+  { header: "Email", key: "email" },
+  { header: "Username", key: "username" },
+];
+
+function thead(t) {
+  let thead = table.createTHead("thead");
+  table.appendChild(thead);
+  let tr = thead.insertRow();
+
+  for (let iterator of headers) {
+    let th = document.createElement("th");
+    th.classList.add("table-th");
+    tr.appendChild(th);
+    let text = document.createTextNode(iterator.header);
+    th.appendChild(text);
+  }
+  let th = document.createElement("th");
+  let text = document.createTextNode("ACTION");
+  th.appendChild(text);
+  th.classList.add("table-th");
+  tr.appendChild(th);
+}
+thead(table);
+console.log(table);
+
+function tbody(t) {
+  let tbody = table.createTBody("tbody");
+  table.appendChild(tbody);
+
+  fetch("http://localhost:3000/cricket")
+    .then((Response) => Response.json())
+    .then((data) => {
+      // console.log(data);
+      for (const element of data) {
+        let tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        tr.classList.add("table-body-tr");
+        for (const iterator of headers) {
+          let td = tr.insertCell();
+          td.classList.add("table-body-td");
+          let text = document.createTextNode(element[iterator["key"]]);
+          console.log(text);
+          td.appendChild(text);
+        }
+        let td = document.createElement("td");
+        tr.appendChild(td);
+        let deleteButton = document.createElement("button");
+        let text1 = document.createTextNode("DELETE");
+        deleteButton.appendChild(text1);
+        td.appendChild(deleteButton);
+        let editButton = document.createElement("button");
+        let text2 = document.createTextNode("Edit");
+        editButton.appendChild(text2);
+        td.appendChild(editButton);
+        td.appendChild(deleteButton);
+        deleteButton.addEventListener("click", (e) => {
+          deleteData(element.id);
+          table.deleteRow(tr.rowIndex);
+        });
+      }
+    });
+}
+
+function deleteData(element) {
+  fetch(`http://localhost:3000/cricket/${element}`, {
+    method: "DELETE",
+  });
+}
+
+tbody(table);
+
+let body = document.querySelector("body");
+body.appendChild(table);
