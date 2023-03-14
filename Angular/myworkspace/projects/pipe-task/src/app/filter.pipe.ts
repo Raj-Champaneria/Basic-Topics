95f327b5c98dfa92601cc58502c5227a8cfbe4b3
Raj Champaneria <raj.champaneria@1Rintern.local>
2023-03-14 18:15:39 +0530

    pipetask

--- Angular/myworkspace/projects/pipe-task/src/app/filter.pipe.ts
+++ Angular/myworkspace/projects/pipe-task/src/app/filter.pipe.ts
@@ -7,9 +7,9 @@ export class FilterPipe implements PipeTransform {
 
   transform(value: any,filterstring:string ){
 
-    // if(filterstring.length===0){
-    //   return value;
-    // }
+    if(filterstring.length===0){
+      return value;
+    }
 
     const array=value.filter((item: any)=>item.name.toLowerCase().includes(filterstring) || item.id.toString().includes(filterstring) || item.salary.toString().includes(filterstring) || item.phone.toString().includes(filterstring));
   
